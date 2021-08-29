b7e3e67077b8197b2e8eaf1d0c477a0233cb3d8d
ensfrknkc <ensfrknkc58@gmail.com>
2021-08-29 18:53:13 +0300

    EntityRelationsComplated

--- EntityLayer/Concrete/Writer.cs
+++ EntityLayer/Concrete/Writer.cs
@@ -14,5 +14,8 @@ namespace EntityLayer.Concrete
         public string WriterImage { get; set; }
         public string WriterMail { get; set; }
         public string WriterPassword { get; set; }
+
+        public ICollection<Heading> Headings { get; set; }
+        public ICollection<Content> Contents { get; set; }
     }
 }
