88c2ac34cd5498eee33b9f3d84b2f54d5d1a1eec
ensfrknkc <ensfrknkc58@gmail.com>
2021-09-13 20:16:15 +0300

    HeadingOperationsStarted

--- BusinessLayer/Abstract/IHeadingService.cs
+++ BusinessLayer/Abstract/IHeadingService.cs
@@ -9,5 +9,10 @@ namespace BusinessLayer.Abstract
 {
     public interface IHeadingService
     {
+        List<Heading> GetList();
+        void HeadingAdd(Heading heading);
+        Heading GetByID(int id);
+        void HeadingDelete(Heading heading);
+        void HeadingUpdate(Heading heading);
     }
 }
--- BusinessLayer/Concrete/HeadingManager.cs
+++ BusinessLayer/Concrete/HeadingManager.cs
@@ -17,6 +17,29 @@ namespace BusinessLayer.Concrete
             _headingDal = headingDal;
         }
 
-       
+        public Heading GetByID(int id)
+        {
+            return _headingDal.Get(x => x.HeadingID == id);
+        }
+
+        public List<Heading> GetList()
+        {
+            return _headingDal.List();
+        }
+
+        public void HeadingAdd(Heading heading)
+        {
+            _headingDal.Insert(heading);
+        }
+
+        public void HeadingDelete(Heading heading)
+        {
+            _headingDal.Delete(heading);
+        }
+
+        public void HeadingUpdate(Heading heading)
+        {
+            _headingDal.Update(heading);
+        }
     }
 }
