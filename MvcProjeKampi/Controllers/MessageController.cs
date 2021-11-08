011373e8b98fd367f61b0cdcdef1927759f99a50
ensfrknkc <ensfrknkc58@gmail.com>
2021-11-08 22:54:23 +0300

    Admin message tab created

---
+++ MvcProjeKampi/Controllers/MessageController.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class MessageController : Controller
+    {
+        MessageManager mm = new MessageManager(new EfMessageDal());
+        public ActionResult Inbox()
+        {
+            var messageList = mm.GetList();
+            return View(messageList);
+        }
+    }
+}
